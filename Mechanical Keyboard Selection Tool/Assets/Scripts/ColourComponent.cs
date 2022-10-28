12180aab7770d1207cc9874ce934caa5a484316e
jenki98 <zaynab.bawa@gmail.com>
2022-10-28 16:12:42 +0100

    Component Structs added

---
+++ Mechanical Keyboard Selection Tool/Assets/Scripts/ColourComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct ModelColours
+{
+    public string colour { get; set; }
+    public int price { get; set; }
+}
+public class ColourComponent : MonoBehaviour
+{
+ 
+
+    void Start()
+    {
+
+    }
+}
+  
